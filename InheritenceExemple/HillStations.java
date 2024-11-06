1c4767c32a1c073b5838880cf45bf7e20bcb7276
VaishnaviJaiswal22 <144608967+VaishnaviJaiswal22@users.noreply.github.com>
2024-11-06 22:45:37 +0530

    Add files via upload

---
+++ InheritenceExemple/HillStations.java
@@ -0,0 +1,13 @@
+package labc3;
+
+public class HillStations {
+	
+	public void location() {
+		System.out.println("Location of Hill Station");
+	}
+	
+	public void famousFor() {
+		System.out.println("Famous for Different Things");
+	}
+
+}
