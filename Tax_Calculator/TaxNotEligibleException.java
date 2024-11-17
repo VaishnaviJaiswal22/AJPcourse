9af4b2a0a0225444efba12562fe6354bf902150d
Vaishnavi Jaiswal <144608967+VaishnaviJaiswal22@users.noreply.github.com>
2024-11-17 23:07:29 +0530

    Add files via upload

---
+++ Tax_Calculator/TaxNotEligibleException.java
@@ -0,0 +1,15 @@
+package labc4;
+
+public class TaxNotEligibleException extends Exception {
+
+	public TaxNotEligibleException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TaxNotEligibleException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
